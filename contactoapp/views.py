dc676b98ddc55bb7bf1ed8aec6b6e6ba7376ea91
Fernando Lozada <blackhorus21@gmail.com>
2022-09-22 16:56:18 -0400

    Formulario de contacto no conectado

--- contactoapp/views.py
+++ contactoapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .forms import FormularioContacto
 
 # Create your views here.
 
 def contacto(request):
-    return render(request, "contacto/contacto.html")
+    
+    formulario=FormularioContacto()
+    
+    
+    
+    
+    return render(request, "contacto/contacto.html", {'formulario':formulario})
+
