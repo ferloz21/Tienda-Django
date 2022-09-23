cc30fc07a895edf347c1e7a771447a46fb6e3519
Fernando Lozada <blackhorus21@gmail.com>
2022-09-22 22:55:39 -0400

    ContactoApp listo (envia email)

--- contactoapp/views.py
+++ contactoapp/views.py
@@ -1,14 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import FormularioContacto
+from django.core.mail import EmailMessage
 
 # Create your views here.
 
 def contacto(request):
     
-    formulario=FormularioContacto()
+    formulario_contacto=FormularioContacto()
+    
+    if request.method=="POST":
+        formulario_contacto=FormularioContacto(data=request.POST)
+        if formulario_contacto.is_valid():
+            Nombre=request.POST.get("Nombre")
+            Email=request.POST.get("Email")
+            Contenido=request.POST.get("Contenido")
+
+            email=EmailMessage("Mensaje desde app django",
+            "Nombre del Usuario: {} Direccion {} Mensaje:\n\n {}".format(Nombre,Email,Contenido),"",["contact.fernandolozada@gmail.com"],reply_to=[Email])
+
+            try:
+                email.send()
+
+                return redirect("/contacto/?valido")
+
+            except:
+                return redirect("/contacto/?error")
+
+
+
     
     
     
-    
-    return render(request, "contacto/contacto.html", {'formulario':formulario})
+    return render(request, "contacto/contacto.html", {'formulario':formulario_contacto})
 
--- proyectoweb/settings.py
+++ proyectoweb/settings.py
@@ -131,3 +131,12 @@ MEDIA_ROOT=BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#configuracion EMAIL
+
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST= "smtp.gmail.com"
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER="contact.fernandolozada@gmail.com"
+EMAIL_HOST_PASSWORD="yrgkpczemctwifkg"
